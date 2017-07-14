a07919d7327499f3fa0146b77cb7c6795f7ac01c
John E <johnelewis92@gmail.com>
2017-07-13 21:57:11 -0500

    update

--- engine.py
+++ engine.py
@@ -2,23 +2,35 @@
 import libtcodpy as libtcod
 #import handle_keys function
 from input_handlers import handle_keys
-
+from entity import Entity
+from render_functions import clear_all, render_all
+from map_objects.game_map import GameMap
 #the main method
 def main():
     #2 variables for screen width and height
     screen_width = 80
     screen_height = 50
+    map_width = 80
+    map_height = 45
+
+    colors = {
+        'dark_wall': libtcod.Color(0, 0, 100),
+        'dark_ground':libtcod.Color(50, 50, 150)
+    }
     
-    #2 variables that allow for adjustment to the player's position and tracking
-    player_x = int(screen_width/2)
-    player_y = int(screen_height/2)
+    player = Entity(int(screen_width / 2), int(screen_height / 2), '@', libtcod.white)
+    npc = Entity(int(screen_width / 2 - 5), int(screen_height / 2), '@', libtcod.yellow)
+    entities = [npc, player]
 
     #detail where the fole file is, and what type
     libtcod.console_set_custom_font('arial10x10.png', libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
     #create the window and give it a height, width, title, and a variable for whether it goes full screen or not
     libtcod.console_init_root(screen_width, screen_height, 'Dare', False)
-    #
+    
     con = libtcod.console_new(screen_width, screen_height)
+    
+    game_map = GameMap(map_width, map_height)
+    
     #2 variables to hold our keyboard and mouse input
     key = libtcod.Key()
     mouse = libtcod.Mouse()
@@ -27,14 +39,10 @@ def main():
         #This function captures events(user input) and updates the appropriate variables
         libtcod.sys_check_for_event(libtcod.EVENT_KEY_PRESS, key, mouse)
         
-        #give the color and location of the ascii symbol
-        libtcod.console_set_default_foreground(con, libtcod.white)
-        #which console where to put it x,y coords, which char to print, and what background it has 
-        libtcod.console_put_char(con, player_x, player_y, '@', libtcod.BKGND_NONE)
-        libtcod.console_blit(con, 0, 0, screen_width, screen_height, 0, 0, 0)
+        render_all(con, entities, game_map, screen_width, screen_height, colors)
         libtcod.console_flush()
         
-        libtcod.console_put_char(con, player_x, player_y, ' ', libtcod.BKGND_NONE)
+        clear_all(con, entities)
         #assign the handle_keys function to the name action
         action = handle_keys(key)
         #all of these check the returned dictionary for a particular key and assign the value to the
@@ -45,8 +53,8 @@ def main():
         #make logical decisions depending on which key is returned
         if move:
             dx, dy = move
-            player_x += dx
-            player_y += dy
+            if not game_map.is_blocked(player.x + dx, player.y +dy):
+                player.move(dx, dy)
         
         if exit:
             return True
